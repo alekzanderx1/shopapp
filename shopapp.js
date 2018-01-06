2db0ede79308edb7c785a6212cd600009d7e8269
Syed Ahmad <ahmadtaqi789@gmail.com>
2018-01-06 11:57:01 +0530

    Search bar added

--- shopapp.js
+++ shopapp.js
@@ -1,9 +1,23 @@
+//create a model to control the app
 var app = angular.module("myShoppingList", []); 
+//create a controller where all the data and methods are defined
 app.controller("myCtrl", function($scope) {
+	//The array where the list items are stored
     $scope.products = ["Create a list ;)","Use the cross to remove me!"];
+	//method called when create button is clicked
 	 $scope.addItem = function () {
-        $scope.products.push($scope.addMe);
+		$scope.errortext="";
+		//case for empty
+        if (!$scope.addMe) {return;}
+		//when no duplicate is there
+		if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+			//when duplicate is found
+            $scope.errortext = "The item is already in your shopping list.";
+        }
     }    
+	//function when item is removed via the remove span element
 	$scope.removeItem = function (x) {
         $scope.products.splice(x, 1);
     } 
